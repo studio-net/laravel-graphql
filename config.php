b5d5d757eeadd5b223ff5cb8083744f134ca8ad4
Cyril Mizzi <cyril@lesiteimmo.com>
2017-07-26 16:19:57 +0200

    adds some options to config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 return [
+	'default_schema' => 'default',
+	'types'   => [],
 	'schemas' => [
 		'default' => [
 			'query'    => [],
@@ -9,5 +11,15 @@ return [
 
 	'types'            => [],
 	'types_as_query'   => true,
-	'types_in_schemas' => 'all' // 'all', 'none', [schema name]
+	'types_in_schemas' => 'all', // 'all', 'none', [schema name]
+
+	'headers' => [],
+	'json_encoding_options' => 0,
+	'variable_input_name' => 'variables',
+
+	'route' => [
+		'prefix'     => 'graphql',
+		'middleware' => [],
+		'controller' => '\\StudioNet\\GraphQL\\GraphQLController@query'
+	]
 ];
