2f96176edb039f6cfff1a8839eb19d21f6bd337e
Cyril Mizzi <cyril@lesiteimmo.com>
2017-07-27 17:17:11 +0200

    removes deprecated unit tests

--- tests/GraphQLTest.php
+++ tests/GraphQLTest.php
@@ -1,14 +1,12 @@
 <?php
 namespace StudioNet\GraphQL\Tests;
 
-use StudioNet\GraphQL\Tests\Entity\User;
 use GraphQL\Type\Definition\Type as GraphQLType;
 use StudioNet\GraphQL\GraphQL;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class GraphQLTest extends TestCase {
 	use DatabaseTransactions;
-	use Traits\EloquentTypeStubTrait;
 
 	/**
 	 * testGetSchemaException
@@ -28,52 +26,5 @@ class GraphQLTest extends TestCase {
 	 */
 	public function testRegisterTypeException() {
 		app(GraphQL::class)->registerType('\\Test\\Class\\Type');
-	}
-
-	/**
-	 * testGetSchema
-	 *
-	 * @return void
-	 */
-	public function testGetSchema() {
-		$stub = $this->getEloquentTypeStub(User::class, 'user', 'a user type');
-		$stub->method('getFields')->willReturn([
-			'name'  => GraphQLType::string(),
-			'email' => GraphQLType::string()
-		]);
-
-		$graphql = app(GraphQL::class);
-		$graphql->registerType($stub);
-		$graphql->registerSchema('default', []);
-
-		$schema = $graphql->getSchema('default');
-		$fields = $schema->getQueryType()->getFields();
-
-		$this->assertArrayHasKey('user', $fields);
-		$this->assertArrayHasKey('users', $fields);
-	}
-
-	/**
-	 * testExecution
-	 *
-	 * @return void
-	 */
-	public function testExecution() {
-		$stub = $this->getEloquentTypeStub(User::class, 'user', 'a user type');
-		$stub->method('getFields')->willReturn([
-			'name'  => GraphQLType::string(),
-			'email' => GraphQLType::string()
-		]);
-
-		factory(User::class, 10)->create();
-
-		$query   = 'query { users(take:2) { name, email }}';
-		$graphql = app(GraphQL::class);
-		$graphql->registerType($stub);
-		$graphql->registerSchema('default', []);
-
-		$response = $graphql->execute($query, [], ['schema' => 'default']);
-		$this->assertSame(2, count($response['data']['users']));
-		$this->assertNotEmpty(array_first($response['data']['users']));
 	}
 }
