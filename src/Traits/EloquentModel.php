9ab4b1386930590b204f10b1e2ea00598652a5cf
Cyril Mizzi <cyril@lesiteimmo.com>
2017-08-03 17:18:48 +0200

    appends cache to EloquentModel in order to improves SQL performances

--- src/Traits/EloquentModel.php
+++ src/Traits/EloquentModel.php
@@ -10,65 +10,75 @@ use ReflectionClass, ReflectionMethod, ErrorException;
  * Implements method `getRelations` and `getColumns`
  */
 trait EloquentModel {
+	/** @var array $columns */
+	private $columns = null;
+
+	/** @var array $relationships */
+	private $relationships = null;
+
 	/**
 	 * Return model relationships
 	 *
 	 * @return array
 	 */
 	public function getRelationship() {
-		$relations  = [];
-		$reflection = new ReflectionClass($this);
-		$traits     = $reflection->getTraits();
-		$exclude    = [];
+		if (is_null($this->relationships)) {
+			$relations  = [];
+			$reflection = new ReflectionClass($this);
+			$traits     = $reflection->getTraits();
+			$exclude    = [];
 
-		// Get traits methods and append them to the excluded methods
-		foreach ($traits as $trait) {
-			foreach ($trait->getMethods() as $method) {
-				$exclude[$method->getName()] = true;
-			}
-		}
-
-		foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-			if ($method->class !== get_class($this)) {
-				continue;
-			}
-
-			// We don't want method with parameters (relationship doesn't have
-			// parameter)
-			if (!empty($method->getParameters())) {
-				continue;
-			}
-
-			// We don't want parsing this current method
-			if (array_key_exists($method->getName(), $exclude)) {
-				continue;
-			}
-
-			try {
-				$return = $method->invoke($this);
-
-				// Get only method that returned Relation instance
-				if ($return instanceof Relation) {
-					$name   = $method->getName();
-					$type   = with(new ReflectionClass($return))->getShortName();
-					$model  = with(new ReflectionClass($return->getRelated()));
-
-					// Assert that relationship field handle this trait :
-					// otherwise, we cannot check columns and relationships
-					if (!array_key_exists(__TRAIT__, $model->getTraits())) {
-						continue;
-					}
-
-					$relations[$name] = [
-						'field' => $method->getName(),
-						'type'  => $type,
-						'model' => $model->getName()
-					];
+			// Get traits methods and append them to the excluded methods
+			foreach ($traits as $trait) {
+				foreach ($trait->getMethods() as $method) {
+					$exclude[$method->getName()] = true;
 				}
-			} catch (ErrorException $e) {}
+			}
+
+			foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+				if ($method->class !== get_class($this)) {
+					continue;
+				}
+
+				// We don't want method with parameters (relationship doesn't
+				// have parameter)
+				if (!empty($method->getParameters())) {
+					continue;
+				}
+
+				// We don't want parsing this current method
+				if (array_key_exists($method->getName(), $exclude)) {
+					continue;
+				}
+
+				try {
+					$return = $method->invoke($this);
+
+					// Get only method that returned Relation instance
+					if ($return instanceof Relation) {
+						$name   = $method->getName();
+						$type   = with(new ReflectionClass($return))->getShortName();
+						$model  = with(new ReflectionClass($return->getRelated()));
+
+						// Assert that relationship field handle this trait :
+						// otherwise, we cannot check columns and relationships
+						if (!array_key_exists(__TRAIT__, $model->getTraits())) {
+							continue;
+						}
+
+						$relations[$name] = [
+							'field' => $method->getName(),
+							'type'  => $type,
+							'model' => $model->getName()
+						];
+					}
+				} catch (ErrorException $e) {}
+			}
+
+			$this->relationships = $relations;
 		}
 
-		return $relations;
+		return $this->relationships;
 	}
 
 	/**
@@ -78,54 +88,58 @@ trait EloquentModel {
 	 * @return array
 	 */
 	public function getColumns() {
-		$data       = [];
-		$table      = $this->getTable();
-		$connection = $this->getConnection();
-		$primary    = $this->getKeyName();
-		$columns    = $connection->getSchemaBuilder()->getColumnListing($table);
+		if (is_null($this->columns)) {
+			$data       = [];
+			$table      = $this->getTable();
+			$connection = $this->getConnection();
+			$primary    = $this->getKeyName();
+			$columns    = $connection->getSchemaBuilder()->getColumnListing($table);
 
-		// Remove hidden columns : we don't want show or update them. Also
-		// append relationships virtual columns
-		$related = $this->getRelationship();
-		$columns = array_diff($columns, $this->getHidden());
-		$columns = array_merge(array_keys($related), $columns);
+			// Remove hidden columns : we don't want show or update them. Also
+			// append relationships virtual columns
+			$related = $this->getRelationship();
+			$columns = array_diff($columns, $this->getHidden());
+			$columns = array_merge(array_keys($related), $columns);
 
-		foreach (array_unique($columns) as $column) {
-			try {
-				$type = $connection->getDoctrineColumn($table, $column);
-				$type = $type->getType();
-			} catch (SchemaException $e) {
-				// There's nothing left to do (it's a virtual field or, it
-				// also could append with PostgreSQL multiple schemas)
-				$data[$column] = null;
-				continue;
+			foreach (array_unique($columns) as $column) {
+				try {
+					$type = $connection->getDoctrineColumn($table, $column);
+					$type = $type->getType();
+				} catch (SchemaException $e) {
+					// There's nothing left to do (it's a virtual field or, it
+					// also could append with PostgreSQL multiple schemas)
+					$data[$column] = null;
+					continue;
+				}
+
+				// Parse each available database data type and call is related
+				// GraphQL type
+				switch ($type->getName()) {
+				case 'smallint'     :
+				case 'bigint'       :
+				case 'integer'      : $type = GraphQLType::int()                         ; break;
+				case 'decimal'      :
+				case 'float'        : $type = GraphQLType::float()                       ; break;
+				case 'date'         :
+				case 'datetimetz'   :
+				case 'time'         :
+				case 'datetime'     : $type = \GraphQL::scalar('timestamp')              ; break;
+				case 'array'        :
+				case 'simple_array' : $type = GraphQLType::listOf(GraphQLType::string()) ; break;
+				default             : $type = GraphQLType::string()                      ; break;
+				}
+
+				// Assert primary key is an id
+				if ($column === $primary) {
+					$type = GraphQLType::id();
+				}
+
+				$data[$column] = $type;
 			}
 
-			// Parse each available database data type and call is related
-			// GraphQL type
-			switch ($type->getName()) {
-			case 'smallint'     :
-			case 'bigint'       :
-			case 'integer'      : $type = GraphQLType::int()                         ; break;
-			case 'decimal'      :
-			case 'float'        : $type = GraphQLType::float()                       ; break;
-			case 'date'         :
-			case 'datetimetz'   :
-			case 'time'         :
-			case 'datetime'     : $type = \GraphQL::scalar('timestamp')              ; break;
-			case 'array'        :
-			case 'simple_array' : $type = GraphQLType::listOf(GraphQLType::string()) ; break;
-			default             : $type = GraphQLType::string()                      ; break;
-			}
-
-			// Assert primary key is an id
-			if ($column === $primary) {
-				$type = GraphQLType::id();
-			}
-
-			$data[$column] = $type;
+			$this->columns = $data;
 		}
 
-		return $data;
+		return $this->columns;
 	}
 }
