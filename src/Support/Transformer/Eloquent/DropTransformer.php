7a35d7469caa120efc31157665166070c236fc27
Cyril Mizzi <cyril@lesiteimmo.com>
2017-11-06 16:49:42 +0100

    adds drop transformer force delete on soft deletes

--- src/Support/Transformer/Eloquent/DropTransformer.php
+++ src/Support/Transformer/Eloquent/DropTransformer.php
@@ -1,6 +1,7 @@
 <?php
 namespace StudioNet\GraphQL\Support\Transformer\Eloquent;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use StudioNet\GraphQL\Support\Transformer\Transformer;
 use StudioNet\GraphQL\Support\Definition\Definition;
 use StudioNet\GraphQL\Definition\Type;
@@ -28,7 +29,16 @@ class DropTransformer extends Transformer {
 	 * @return array
 	 */
 	public function getArguments(Definition $definition) {
-		return [
+		$args   = [];
+		$traits = class_uses($definition->getSource());
+
+		if (in_array(SoftDeletes::class, $traits)) {
+			$args = [
+				'force' => ['type' => Type::bool(), 'description' => 'Force deletion']
+			];
+		}
+
+		return $args + [
 			'id' => ['type' => Type::nonNull(Type::id()), 'description' => 'Primary key lookup' ]
 		];
 	}
@@ -51,7 +61,12 @@ class DropTransformer extends Transformer {
 	 */
 	public function getResolver(array $opts) {
 		$model = $opts['source']->findOrFail(array_get($opts['args'], 'id', 0));
-		$model->delete();
+
+		if (array_get($opts['args'], 'force', false)) {
+			$model->forceDelete();
+		} else {
+			$model->delete();
+		}
 
 		return $model;
 	}
