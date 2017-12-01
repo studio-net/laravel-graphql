c8bd7b21cd6c36552949c6835b4933aeaeaa2299
Cyril Mizzi <cyril@lesiteimmo.com>
2017-12-01 13:36:53 +0100

    adds relationship resolution from mutation

--- src/Support/Transformer/Eloquent/StoreTransformer.php
+++ src/Support/Transformer/Eloquent/StoreTransformer.php
@@ -4,6 +4,7 @@ namespace StudioNet\GraphQL\Support\Transformer\Eloquent;
 use StudioNet\GraphQL\Support\Transformer\Transformer;
 use StudioNet\GraphQL\Support\Definition\Definition;
 use StudioNet\GraphQL\Definition\Type;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * Transform a Definition into create/update mutation
@@ -55,15 +56,34 @@ class StoreTransformer extends Transformer {
 	 */
 	public function getResolver(array $opts) {
 		$model = $opts['source']->findOrNew(array_get($opts['args'], 'id', 0));
-		$data  = array_filter($opts['args']['with'], function($key) use ($model) {
-			return !method_exists($model, $key);
-		}, ARRAY_FILTER_USE_KEY);
+		$data  = array_filter($opts['args']['with'], function ($value, $key) use ($model) {
+			return !(is_array($value) and method_exists($model, $key));
+		}, ARRAY_FILTER_USE_BOTH);
 
 		$model->fill($data);
 		$model->save();
 
 		foreach (array_diff_key($opts['args']['with'], $data) as $column => $values) {
-			$model->{$column}()->sync(array_filter($values));
+			$relation = $model->{$column}();
+
+			// If we are on a hasOne relationship, we have to manage the
+			// firstOrNew case
+			//
+			// https://laracasts.com/discuss/channels/general-discussion/hasone-create-duplicates
+			if (get_class($relation) === HasOne::class) {
+				$relation->firstOrNew([])->fill($values)->save();
+			}
+
+			else {
+				if (!is_array(array_first($values))) {
+					$values = [$values];
+				}
+
+				// For each relationship, find or new by id and fill with data
+				foreach ($values as $value) {
+					$relation->findOrNew(array_get($values, 'id', null))->fill($value)->save();
+				}
+			}
 		}
 
 		return $model;
