9ec1416d2c9a403936a2dd87c0b6ac99f495eae1
lorado <eugen.sacharow@gmx.de>
2018-08-06 01:38:44 +0200

    improve guessing relations

--- src/Support/Transformer/Transformer.php
+++ src/Support/Transformer/Transformer.php
@@ -1,12 +1,13 @@
 <?php
 namespace StudioNet\GraphQL\Support\Transformer;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Application;
 use StudioNet\GraphQL\Support\Definition\Definition;
 use GraphQL\Type\Definition\ResolveInfo;
 use StudioNet\GraphQL\Cache\Cachable;
 use StudioNet\GraphQL\Cache\CachePool;
-use GraphQL\Type\Definition\InputObjectType;
 
 /**
  * Define transformer base class
@@ -72,8 +73,12 @@ abstract class Transformer extends Cachable {
 		$app = $this->app;
 
 		return function ($root, array $args, $context, ResolveInfo $info) use ($definition, $app) {
-			$fields = $info->getFieldSelection(3);
 			$reflect = new \ReflectionClass($this);
+
+			$isListTransformer = $reflect->getShortName() === "ListTransformer";
+			$fieldsDepth = $isListTransformer ? 4 : 3; // may be increase depth?
+			$fields = $info->getFieldSelection($fieldsDepth);
+
 			$definition->assertAcl(
 				str_replace("transformer", "", strtolower($reflect->getShortName())),
 				[
@@ -84,13 +89,15 @@ abstract class Transformer extends Cachable {
 				]
 			);
 
+			$fieldsForGuessingRelations = $isListTransformer ? $fields['items'] : $fields;
+
 			$opts = [
 				'root' => $root,
 				'args' => array_filter($args),
 				'fields' => $fields,
 				'context' => $context,
 				'info' => $info,
-				'with' => $this->guessWithRelations($definition, $fields),
+				'with' => $this->guessWithRelations($this->app->make($definition->getSource()), $fieldsForGuessingRelations),
 				'source' => $app->make($definition->getSource()),
 				'rules' => $definition->getRules(),
 				'filterables' => $definition->getFilterable(),
@@ -102,24 +109,29 @@ abstract class Transformer extends Cachable {
 	}
 
 	/**
-	 * Return relationship based on entity definition construction
+	 * Return relationship based on fields that are queried
 	 *
-	 * @param  Definition $definition
+	 * @param  Model $model
 	 * @param  array $fields
+	 * @param  string $parentRelation
+	 *
 	 * @return array
 	 * @SuppressWarnings(PHPMD.UnusedFormalParameter)
 	 */
-	public function guessWithRelations(Definition $definition, array $fields) {
+	public function guessWithRelations(Model $model, array $fields, string $parentRelation = null) {
 		$relations = [];
-		$source = $this->app->make($definition->getSource());
-
 		// Parse each field in order to retrieve relationship elements on root
 		// of array (as relationship are based upon multiple resolvers, we just
 		// have to handle the root fields here)
 		foreach ($fields as $key => $field) {
-			// TODO Improve this checker
-			if (is_array($field) and method_exists($source, $key)) {
-				array_push($relations, $key);
+			if (is_array($field) && method_exists($model, $key)) {
+				// get relation name and store it
+				/** @var BelongsToMany $relation */
+				$relation = call_user_func([$model, $key]);
+				$relationNameToStore = $parentRelation ? "{$parentRelation}.{$relation->getRelationName()}" : $relation->getRelationName();
+				$relations[] = $relationNameToStore;
+				// also guess relations for found relation
+				$relations = array_merge($relations, $this->guessWithRelations($relation->getModel(), $field, $relationNameToStore));
 			}
 		}
 
